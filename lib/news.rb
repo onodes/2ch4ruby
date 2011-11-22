f6fd84aa237a0c6ba6fb0d5374b6b78d9ceb0537
Daichi ONODERA <onodes@onod.es>
2011-11-22 18:29:42 +0900

    Thre class

--- lib/news.rb
+++ lib/news.rb
@@ -2,6 +2,7 @@
 #encoding: utf-8
 require "mechanize"
 require "time"
+require "./thre"
 #hogehoge
 #hugahuga
 agent = Mechanize.new
@@ -13,37 +14,12 @@ url = array2.map{|link| "http://uni.2ch.net/test/read.cgi/news/"+link}
 p url.size
 list = []
 20.times{|i|
-  page2 = agent.get(url[i])
+  #page2 = agent.get(url[i])
+  
+  t = Thre.new(url[i])
+  p t.title
+  p t.impetus
 
-  #スレタイ
-  title = (page2/"h1").text
-  #print title + ""
-
-  #name
-  name = page2/"dt"
-  #print "(" + name.size.to_s + ") "
-
-  #res
-  res = page2/"dd"
-  res = res.map{|link| link.text}
-
-  name = name.map{|link| link.text}
-  name = name.map{|link| link.split("：")}
-  no = name.map{|link| link[0]}
-  hn = name.map{|link| link[1]}
-  id = name.map{|link| link[2].split(" ")}.map{|link| link[2]}
-  date = name.map{|link| link[2].split(" ")}.map{|link| link[0]}.map{|link| link.sub(/\(.\)/,"")}
-  time = name.map{|link| link[2].split(" ")}.map{|link| link[1]}
-  day = date[0] + " " + time[0]
-
-  #勢い
-  imp = res.size/(Time.now - Time.parse(day))*60*60*24
-  #p imp
-
-  list2 = []
-  list2.push(imp)
-  list2.push(title)
-  list.push(list2)
 
 =begin
 (name.size-1).times{|i|
